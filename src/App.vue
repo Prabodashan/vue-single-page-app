88f4d8d61572de469f2ce5604f6cb6a43a6659c2
Prabodashan <prabodashan14@gmail.com>
2023-03-10 10:17:29 +0530

    completed main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>
-    <p>simple text</p>
-  </div>
+  <Navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></Navbar>
+  <PageViewer :page="pages[activePage]"></PageViewer>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+import PageViewer from "./components/PageViewer.vue";
+
 export default {
+  components: {
+    Navbar,
+    PageViewer,
+  },
   data() {
     return {
       activePage: 0,
